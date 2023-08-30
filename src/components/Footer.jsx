bb7e6051ec9d369f658e1fabbabbfc037597ed53
freudinsky <timo.fuchs94@icloud.com>
2023-08-30 15:29:00 +0200

    sortbar+footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+	return (
+		<>
+			<div className="footer">
+				<ul id="footer-ul">
+					<li>
+						<a href="#">Imprint</a>
+					</li>
+					<li>
+						<a href="#">Privacy note</a>
+					</li>
+					<li>
+						<a href="#">Sources</a>
+					</li>
+				</ul>
+				<p>© 2023 HackerNews by Ayla, Phil and Timo</p>
+			</div>
+		</>
+	);
+}
